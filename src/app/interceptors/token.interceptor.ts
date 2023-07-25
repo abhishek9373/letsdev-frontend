5888961902ea87e16c7b8ff83c6a3e6858324367
abhishek9373 <abhishekgund500@gmail.com>
2023-07-25 22:13:20 +0530

    removed token from for file upload

--- src/app/interceptors/token.interceptor.ts
+++ src/app/interceptors/token.interceptor.ts
@@ -15,6 +15,14 @@ export class TokenInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+    const omitTokenAttachment = request.url.includes('/storage.googleapis.com');
+
+    // If the token attachment is not needed, pass the original request without changes.
+    if (omitTokenAttachment) {
+      return next.handle(request);
+    }
+
     const authToken = localStorage.getItem('authToken');
     if(authToken){
       const authRequest = request.clone({
