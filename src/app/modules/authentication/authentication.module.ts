1efb7d09e2c694465545f7b7dd492250fae0ad7b
abhishek9373 <abhishekgund500@gmail.com>
2023-07-27 04:39:09 +0530

    update

--- src/app/modules/authentication/authentication.module.ts
+++ src/app/modules/authentication/authentication.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DomainselectionComponent } from './domainselection/domainselection.component';
 import { OnboardComponent } from './onboard/onboard.component';
@@ -6,18 +6,32 @@ import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 
 import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { EmailverificationComponent } from './emailverification/emailverification.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptor } from 'src/app/interceptors/token.interceptor';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/models/user.model';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 
 @NgModule({
   declarations: [
     DomainselectionComponent,
     LoginComponent,
-    OnboardComponent
+    OnboardComponent,
+    EmailverificationComponent
   ],
   imports: [
     CommonModule,
     AuthenticationRoutingModule,
-    FormsModule
-  ]
+    FormsModule,
+    MatProgressSpinnerModule
+  ],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true},
+    AuthService,
+    UserService,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 })
 export class AuthenticationModule { }
