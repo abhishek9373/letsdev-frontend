f2535edb4695eaa403a779cd69985939ef767787
abhishek9373 <abhishekgund500@gmail.com>
2023-07-26 13:31:28 +0530

    get initial posts

--- src/app/modules/posts/posts/posts.component.ts
+++ src/app/modules/posts/posts/posts.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
 
+  posts: any = [];
+
+  constructor (private postService: PostService){}
+  ngOnInit(): void {
+    this.postService.bootStrap().subscribe((data: any)=>{
+      this.posts.push(...data.data);
+      console.log(this.posts);
+    })
+  }
 }
