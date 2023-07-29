a7f7cd5ef29439266cdd8afc8429f330d40fc8db
abhishek9373 <abhishekgund500@gmail.com>
2023-07-29 11:33:27 +0530

    added login guard

---
+++ src/app/guard/login.guard.ts
@@ -0,0 +1,36 @@
+import { CanActivate } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { Injectable } from '@angular/core';
+import { UserService } from '../models/user.model';
+import { UserService as UserModelService } from '../services/user.service';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuard implements CanActivate {
+  constructor(private authService: AuthService, private userService: UserService, private router: Router, private userModelService: UserModelService) { }
+  async canActivate(route: any, state: any): Promise<boolean> {
+    const token: string | null = localStorage.getItem('authToken');
+    if (!token) {
+      return true;
+    }
+    this.router.navigate(['/auth']);
+    return false;
+  //   try {
+  //     const data = await this.userModelService.getOnly().toPromise();
+
+  //     if (!data) {
+  //       return true;
+  //     } else {
+  //       this.router.navigate(['/posts']);
+  //       return false;
+  //     }
+  //   } catch (error) {
+  //     // Handle any errors that might occur during the API call
+  //     return true;
+  //   }
+  // }
+    return true;
+  }
+}
