ffd001b15690b00e99edb2f8d89257bf1644b3a3
abhishek9373 <abhishekgund500@gmail.com>
2023-08-02 01:46:40 +0530

    comment interface

---
+++ src/app/interfaces/Comment.interface.ts
@@ -0,0 +1,14 @@
+export interface commentResponse {
+  data: Array<comment>
+}
+
+export interface comment {
+    _id: string,
+    text: string,
+    preferences : { likes: number, dislikes: number },
+    user : { name: string, _id: string },
+    createdAt: Date,
+    interaction : { isliked: boolean, isdisliked: boolean },
+    likes: number,
+    dislikes: number
+}
