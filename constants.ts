2bbc018c8c9f905211e9be1ffa406389bd20f8a2
abhishek9373 <abhishekgund500@gmail.com>
2023-08-28 15:56:49 +0530

    added shared constants

---
+++ constants.ts
@@ -0,0 +1,10 @@
+// production
+export const SOCKET_HOST: string = "https://devbuilder.tech/services";
+export const BASE_URL: string = "https://devbuilder.tech/services";
+
+
+
+
+// developemet
+// export const SOCKET_HOST: string = "http://localhost:3015";
+// export const BASE_URL: string = "http://localhost:3015";
